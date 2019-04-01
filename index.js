6a40e4ea0dbd0b973b6a6e2e842e731f2f108017
Anthea Merton <anthea.merton@gmail.com>
2019-04-01 15:04:30 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// function takeANumber(line, name) {
+// line.push(name);
+// return `Welcome, ${name}. You are number ${line.length} in line.`;
+// }
+var i = 0
+function takeANumer(line) {
+  line.push(i++)
+  
+return `now serving ${i}
